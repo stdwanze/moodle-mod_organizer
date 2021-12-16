339629906153a6a8f0b4cfdf1de9c82922fa2db5
Stefan Eggers <stefan.eggers@ibsolution.de>
2021-12-16 15:47:15 +0100

    fix2

--- view_action_form_assign.php
+++ view_action_form_assign.php
@@ -151,7 +151,7 @@ class organizer_assign_slot_form extends moodleform
             }
                 // if user is in other slot in same timeframe
                $userapps = organizer_get_all_userappointments_for_time($slot->starttime + ($slot->duration/2), $userid);
-               if(count($userapps) > 0) return false;
+               if(count($userapps) > 0) { return false;}
                return true;
         }
 
